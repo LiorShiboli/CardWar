c19920df6210fe036e7bb1fb38cc12c930b69acb
Lior Shiboli <lior12sh@gmail.com>
2023-03-28 17:07:13 +0300

    added card empty class

---
+++ Test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+using namespace std;
+
+#include "sources/player.hpp"
+#include "sources/game.hpp"
+#include "sources/card.hpp"
+using namespace ariel;
